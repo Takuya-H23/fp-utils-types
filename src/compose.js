dc88d03f4e6854f54dd755506e3599009920f1c5
Takuya Hirata <takuyahirata4@gmail.com>
2020-09-06 13:54:21 -0400

    fix: make constants for  error messages

--- src/compose.js
+++ src/compose.js
@@ -1,5 +1,7 @@
+export const ERROR_NEED_A_FUNCTION = "Compose needs at least one function"
+
 const compose = (...fns) => x => {
-  if (fns.length <= 0) throw new Error("Compose needs at least one function")
+  if (fns.length <= 0) throw new Error(ERROR_NEED_A_FUNCTION)
   return fns.reduceRight((acc, fn) => fn(acc), x)
 }
 
--- test/pipe.test.js
+++ test/pipe.test.js
@@ -1,11 +1,12 @@
 import { pipe } from "../src"
+import { ERROR_NEED_A_FUNCTION } from "../src/compose"
 import { toUpper, exclaim, smile } from "./testUtils"
 
 const piped = pipe(toUpper, exclaim, smile)
 
 describe("pipe", () => {
   test("should throw an error when no functions are passed", () => {
-    expect(pipe()).toThrow()
+    expect(pipe()).toThrow(ERROR_NEED_A_FUNCTION)
   })
 
   test("should return composed function", () => {
