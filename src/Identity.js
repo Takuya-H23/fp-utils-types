2a848bc11716cbc1d5fe070ae3cd3720ea4d3d3d
Takuya Hirata <takuyahirata4@gmail.com>
2020-12-27 16:35:05 -0500

    refactor

--- src/Identity.js
+++ src/Identity.js
@@ -1,12 +1,3 @@
-// const Identity = x => ({
-//   x,
-//   map: f => Identity(f(x)),
-//   chain: f => f(x),
-//   concat: other => Identity(x.concat(other.x)),
-//   fold: f => f(x),
-// })
-
-// Identity.of = x => Identity(x)
 class Identity {
   constructor(x) {
     this._x = x
