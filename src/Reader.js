b59a87a46821d8d6bf413d87ad7507f6707ce093
Takuya Hirata <takuyahirata4@gmail.com>
2021-01-13 07:23:44 -0500

    feat(wip): Reader with fn

--- src/Reader.js
+++ src/Reader.js
@@ -1,8 +1,11 @@
 const Reader = run => ({
   run,
   map: f => Reader(x => f(run(x))),
+  chain: f => Reader(x => f(run(x)).run(x)),
   concat: other => Reader(x => run(x).concat(other.run(x))),
-  fold: x => run(x),
 })
+
+Reader.of = x => Reader(() => x)
+Reader.ask = Reader(x => x)
 
 export default Reader
--- test/Reader.test.js
+++ test/Reader.test.js
@@ -1,5 +1,5 @@
 import Reader from "../src/Reader"
-import { add, smile } from "./testUtils"
+import { add, smile, toUpper } from "./testUtils"
 
 const add1 = x => add(1)(x)
 
@@ -11,4 +11,24 @@ test("should map", () => {
 test("should concat", () => {
   const test = Reader(smile).concat(Reader(smile)).run("hi")
   expect(test).toEqual("hi :)hi :)")
+})
+
+test("should chain", () => {
+  const test = Reader(toUpper)
+    .chain(upper => Reader(y => [upper, y]))
+    .run("hi")
+  expect(test).toEqual(["HI", "hi"])
+})
+
+test('should work with "of"', () => {
+  const test = Reader.of("hi").map(toUpper).run()
+  expect(test).toBe("HI")
+})
+
+test("should be able to ask ", () => {
+  const test = Reader.of("hi")
+    .map(toUpper)
+    .chain(upper => Reader.ask.map(y => [upper, y]))
+    .run("hello")
+  expect(test).toEqual(["HI", "hello"])
 })
